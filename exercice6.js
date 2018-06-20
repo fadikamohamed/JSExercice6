6e7f292b99ac92f31c947b7005524ce4b57eedab
fadikamohamed <fadika.mohamed02@gmail.com>
2018-06-20 12:36:30 +0200

    Exercice 6 terminé

---
+++ exercice6.js
@@ -0,0 +1,6 @@
+var entreNombre1 = prompt('Entrez premier nombre a virgule');
+nombre1 = parseInt(entreNombre1);
+var entreNombre2 = prompt('Entrez deuxieme nombre a virgule');
+nombre2 = parseFloat(entreNombre2);
+result = nombre1 / nombre2;
+alert(result);
