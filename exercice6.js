eb43086251de9e6aed4eac3da628924807d2edae
fadikamohamed <fadika.mohamed02@gmail.com>
2018-06-21 10:43:50 +0200

    Exercice 6 vraiment terminé cette fois

--- exercice6.js
+++ exercice6.js
@@ -1,6 +1,25 @@
-var entreNombre1 = prompt('Entrez premier nombre a virgule');
-nombre1 = parseInt(entreNombre1);
-var entreNombre2 = prompt('Entrez deuxieme nombre a virgule');
-nombre2 = parseFloat(entreNombre2);
-result = nombre1 / nombre2;
-alert(result);
+//Délaration de la fonction.
+function numbers() {
+  //Déclaration des variables.
+  var firstNumber = document.getElementById('firstNumber').value;
+  var secondNumber = document.getElementById('secondNumber').value;
+  //Délaration du régex et intégration dans une variable.
+  var regex = /^[0-9\.]+$/;
+  //Déclaration de la condition
+  if ((regex.test(firstNumber) == true) && (regex.test(secondNumber) == true))
+   {
+    //Conversion de la chaine de caractère en nombre entier et intégration dans une variable.
+    var firstNumberInt = parseInt(firstNumber);
+    //Conversion de la chaine de caractère en nombre décimal et intégration dans une variable.
+    var secondNumberInt = parseFloat(secondNumber);
+    //Divsion des deux nombres obtenu.
+    var result = firstNumberInt / secondNumberInt;
+    //Boite de dialogue avec appel des variables.
+    alert(firstNumber + ' / ' + secondNumber + ' = ' + result);
+   }else
+    {
+    //Boite de dialogue avec message d'érreur.
+    alert('Entré invalide !');
+    }
+
+}
